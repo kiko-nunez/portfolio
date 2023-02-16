d8e634f0600fd09494d7370137b4d351fa13b648
kiko-nunez <117489647+kiko-nunez@users.noreply.github.com>
2023-02-16 15:53:28 -0700

    buils front page with pfp and image icons

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Head from 'next/head'
 import {BsFillMoonStarsFill} from 'react-icons/bs'
+import {AiFillLinkedin, AiFillGithub} from 'react-icons/ai'
+import Image from 'next/image'
+import pfp from '../public/pfp.png'
 
 export default function Home() {
   return (
@@ -13,13 +16,24 @@ export default function Home() {
       <main>
       <section className='bg-blue-900 min-h-screen'>
         <nav className='py-10 mb-12 flex justify-between'>
-          <h1 className='text-xl'>Kiko Nunez Portfolio</h1>
+          <h1 className='text-xl font-burtons'>Kiko Nunez Portfolio</h1>
           <ul class="list-none flex items-center">
           <li><BsFillMoonStarsFill className='cursor-pointer text-2xl'/></li>
           <li><a href='#' className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-lg ml-8'> Resume </a></li>
         </ul>
         </nav>
-
+      <div className='text-center p-10'>
+        <h2 className='text-5xl py-2 text-teal-600 font-medium'>Kiko Nunez</h2>
+        <h3 className='text-2xl py-2'>Software Developer</h3>
+        <p className='text-md py-5 leading-8 text-white-800'>Full-Stack developer providing services. Join me down below!</p>
+      </div>
+      <div className='text-5xl flex justify-center gap-16 py-3'>
+        <AiFillLinkedin className=''/>
+        <AiFillGithub/>
+      </div>
+      <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-60 h-60 mt-20 overflow-hidden'>
+        <Image src={pfp} layout="fill" objectFit='cover'/>
+      </div>
       </section>
 
       </main>
