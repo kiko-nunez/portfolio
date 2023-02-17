521d68338e8ad1d926556a1363d1fe2a7bab0bad
kiko-nunez <117489647+kiko-nunez@users.noreply.github.com>
2023-02-17 00:58:07 -0700

    removes single quotes so that deployment clears errors

--- pages/index.js
+++ pages/index.js
@@ -149,46 +149,47 @@ export default function Home() {
             <h2 className="text-center text-4xl py-1 mt-4 ">Contact</h2>
 
             <form class="max-w-lg mx-auto p-4">
-  <div class="mb-4">
-    <label class="block text-white-700 font-bold mb-2" htmlFor="name">
-      Name
-    </label>
-    <input
-      class="appearance-none border rounded w-full py-2 px-3 text-white-700 leading-tight focus:outline-none focus:shadow-outline"
-      id="name"
-      type="text"
-      placeholder="Enter your name"
-    />
-  </div>
-  <div class="mb-4">
-    <label class="block text-white-700 font-bold mb-2" htmlFor="email">
-      Email
-    </label>
-    <input
-      class="appearance-none border rounded w-full py-2 px-3 text-white-700 leading-tight focus:outline-none focus:shadow-outline"
-      id="email"
-      type="email"
-      placeholder="Enter your email address"
-    />
-  </div>
-  <div class="mb-4">
-    <label class="block text-white-700 font-bold mb-2" htmlFor="message">
-      Message
-    </label>
-    <textarea
-      class="appearance-none border rounded w-full py-2 px-3 text-white-700 leading-tight focus:outline-none focus:shadow-outline"
-      id="message"
-      placeholder="Enter your message"
-    ></textarea>
-  </div>
-  <div class="flex items-center justify-center">
-    <button
-      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-      type="button"
-    >
-      Send
-    </button>
-  </div>
+            <div className="mb-4">
+  <label className="block text-white-700 font-bold mb-2" htmlFor="name">
+    Name
+  </label>
+  <input
+    className="appearance-none border rounded w-full py-2 px-3 text-white-700 leading-tight focus:outline-none focus:shadow-outline"
+    id="name"
+    type="text"
+    placeholder="Enter your name"
+  />
+</div>
+<div className="mb-4">
+  <label className="block text-white-700 font-bold mb-2" htmlFor="email">
+    Email
+  </label>
+  <input
+    className="appearance-none border rounded w-full py-2 px-3 text-white-700 leading-tight focus:outline-none focus:shadow-outline"
+    id="email"
+    type="email"
+    placeholder="Enter your email address"
+  />
+</div>
+<div className="mb-4">
+  <label className="block text-white-700 font-bold mb-2" htmlFor="message">
+    Message
+  </label>
+  <textarea
+    className="appearance-none border rounded w-full py-2 px-3 text-white-700 leading-tight focus:outline-none focus:shadow-outline"
+    id="message"
+    placeholder="Enter your message"
+  ></textarea>
+</div>
+<div className="flex items-center justify-center">
+  <button
+    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+    type="button"
+  >
+    Send
+  </button>
+</div>
+
 </form>
 
             </div>
